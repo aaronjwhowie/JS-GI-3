1c909c98a8135761425868b0b9de410a5c3fafd2
aaronjwhowie <aaronjwhowie@gmail.com>
2022-10-01 11:56:21 -0400

    Yum

---
+++ EASY/easy.js
@@ -0,0 +1,11 @@
+function run() {
+  console.log("Todays exercise: runining");
+}
+
+var run = exercise("running");
+
+console.log(run()); // prints "Today's exercise: running"
+
+var swim = exercise("swimming");
+
+console.log(swim()); // prints "Today's exercise: swimming"
